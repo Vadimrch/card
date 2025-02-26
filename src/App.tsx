43515c3570d7ef6f96e1f4fe93e6487c9f049598
KorolenkoVadim <korolenkovadim@gmail.com>
2025-02-26 21:01:45 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+Hello
+      </div>
+
+    </>
+  )
+}
+
+export default App
+
+
+const StyleBtn = styled
