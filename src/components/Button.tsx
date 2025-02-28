d35b31f2e5c89accf6af1534eae95b085c7225a3
KorolenkoVadim <korolenkovadim@gmail.com>
2025-02-28 22:22:01 +0300

    11111

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const StyledBtn = styled.button`
+    
+   background-color: #4E71FE;
+    padding: 10px 20px;
+    border-radius: 5px;
+    font-size: 10px;
+    border: none;
+    color: #ffff;
+    margin-right: 12px;
+    font-weight: 700;
+    width: 86px;
+    white-space: nowrap;
+    display: inline-block;
+`
+export const SuperButton = styled(StyledBtn)`
+background-color: #fff;
+    border: 2px solid #4E71FE;
+    border-radius: 5px;
+    color: 4E71FE;
+`
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,23 @@
 // import React from 'react';
 import desertImage from './../assets/desert.svg'
 import {StyledCard} from './Wrapper.tsx'
-import {DesertImages} from "./Imgs.tsx";
-
+import {DesertImages} from './Imgs.tsx';
+import {StyledBtn} from './Button.tsx';
+import {SuperButton} from './Button.tsx';
+import {Titles} from './Titles.tsx';
+import {Descrip} from './Text.tsx';
 
 export const Card = () => {
     return (
         <StyledCard>
 <DesertImages src={desertImage} alt="Desert"/>
+            <Title>Headline</Title>
+            <Descrip>
+                Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen
+            </Descrip>
+<StyledBtn>See more</StyledBtn>
+<SuperButton>Save</SuperButton>
+
         </StyledCard>
     );
 };
